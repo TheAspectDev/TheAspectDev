8a2ce8d37242b7e9691e8db250de64e1513909aa
TheAspectDev <95575356+TheAspectDev@users.noreply.github.com>
2023-06-11 19:23:19 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,5 @@
 
 ## 🖥 Github
 
-<img>https://github-readme-stats.vercel.app/api?username=theaspectdev&show_icons=true&theme=shadow_red)</img>
-
-<img>https://github-readme-stats.vercel.app/api/top-langs/?username=theaspectdev&layout=donut-vertical)](https://github.com/theaspectdev/github-readme-stats</img>
+![Aspect](https://github-readme-stats.vercel.app/api?username=theaspectdev&show_icons=true&theme=shadow_red)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=theaspectdev&layout=donut-vertical)](https://github.com/theaspectdev/github-readme-stats)
